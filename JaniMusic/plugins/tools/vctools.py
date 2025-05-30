fa75934d911c69e88c6201f768b6bf26a7ace73e
ramrk399 <ramavtarchauhan2000@gmail.com>
2025-05-30 22:39:12 +0530

    Update vctools.py

--- JaniMusic/plugins/tools/vctools.py
+++ JaniMusic/plugins/tools/vctools.py
@@ -8,7 +8,8 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.raw.functions.phone import CreateGroupCall, DiscardGroupCall
 from pyrogram.raw.types import InputGroupCall
 from JaniMusic.utils.database import get_assistant
-from telethon.tl.functions.phone import (
+from telethon.tl.functions.phone import 
+(
     CreateGroupCallRequest,
     DiscardGroupCallRequest,
     GetGroupCallRequest,
